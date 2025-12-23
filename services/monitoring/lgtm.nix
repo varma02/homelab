b9235f209f7ecc491f742601408f625594c1cddd
varma02 <varma02.marci@gmail.com>
2025-12-23 10:40:52 +0000

    add grafana alloy

--- services/monitoring/lgtm.nix
+++ services/monitoring/lgtm.nix
@@ -20,17 +20,35 @@
         image = "grafana/grafana:latest";
         volumes = [
           "/persist/monitoring/grafana:/var/lib/grafana"
-          "${./datasources.yaml}:/etc/grafana/provisioning/datasources/datasources.yaml"
+          "${./datasources.yaml}:/etc/grafana/provisioning/datasources/datasources.yaml:ro"
         ];
         networks = [ "monitoring" ];
         ports = [ "80:3000" ];
         dependsOn = [ "prometheus" "loki" "tempo" ];
       };
+      # --- Alloy ---
+      alloy = {
+        image = "grafana/alloy:latest";
+        volumes = [
+          "${./config.alloy}:/etc/alloy/config.alloy:ro"
+          "/proc:/host/proc:ro"
+          "/sys:/host/sys:ro"
+          "/:/host/root:ro"
+        ];
+        networks = [ "monitoring" ];
+        # priviliged = true;
+        cmd = [
+          "run"
+          "--server.http.listen-addr=0.0.0.0:12345"
+          "--storage.path=/var/lib/alloy/data"
+          "/etc/alloy/config.alloy"
+        ];
+      };
       # --- Prometheus ---
       prometheus = {
         image = "prom/prometheus:latest";
         volumes = [
-          "${./prometheus.yaml}:/etc/prometheus/prometheus.yml"
+          "${./prometheus.yaml}:/etc/prometheus/prometheus.yml:ro"
           "/persist/monitoring/prometheus:/prometheus"
         ];
         networks = [ "monitoring" ];
@@ -39,13 +57,14 @@
           "--storage.tsdb.path=/prometheus" 
           "--web.console.libraries=/usr/share/prometheus/console_libraries"
           "--web.console.templates=/usr/share/prometheus/consoles"
+          "--web.enable-remote-write-receiver"
         ];
       };
       # --- Loki ---
       loki = {
         image = "grafana/loki:latest";
         volumes = [
-          "${./loki.yaml}:/etc/loki/local-config.yaml"
+          "${./loki.yaml}:/etc/loki/local-config.yaml:ro"
           "/persist/monitoring/loki:/tmp/loki"
         ];
         networks = [ "monitoring" ];
