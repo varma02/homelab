6ba6ef89eef7f922907b88133d42b1bdd14db01b
varma02 <varma02.marci@gmail.com>
2025-12-23 10:00:17 +0000

    docker permission fix maybe

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,4 +1,12 @@
 {
+  systemd.services.init-monitoring-network = {
+    description = "Docker persist permission fix";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Type = "oneshot";
+    script = "chmod -R 777 /persist";
+  };
+
   virtualisation.docker = {
     enable = true;
     autoPrune = {
